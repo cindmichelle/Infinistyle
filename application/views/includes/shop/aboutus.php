a4590f8f00f02a06505853766002f402181f061b
azurast <azura.sakan@student.umn.ac.id>
2019-05-18 13:01:15 +0700

    About Us Page
    - kubikin about us page, tinggal tambahin foto + nim
    - controller sign up & sign in yg di dlm folder shop di delete krn ga 
    dibutuhin lagi, ada di folder user

---
+++ application/views/includes/shop/aboutus.php
@@ -0,0 +1,41 @@
+<body>
+    <div class="container">
+        <h2 class="text-success">Our Front End Team</h2>
+        <div class="row" id="fontEnd">
+          <div class="col-sm-4 col-6 mt-4 mt-sm-0">
+                <img src="<?php echo base_url('argon\assets\img\theme\team-1-800x800.jpg');?>" alt="Raised image" class="img-fluid rounded shadow-lg" style="width: 150px;">
+                <small class="d-block text-uppercase text-white font-weight-bold">Steve Manumpil</small>
+                <small class="d-block text-uppercase text-white font-weight-bold mb-4">000 000 xxxxx</small>
+          </div>
+          <div class="col-sm-4 col-6 mt-4 mt-sm-0">
+                <img src="<?php echo base_url('argon\assets\img\theme\team-1-800x800.jpg');?>" alt="Raised image" class="img-fluid rounded shadow-lg" style="width: 150px;">
+                <small class="d-block text-uppercase text-white font-weight-bold">Azura Sakan Taufik</small>
+                <small class="d-block text-uppercase text-white font-weight-bold mb-4">000 000 20528</small>
+          </div>
+          <div class="col-sm-4 col-6 mt-4 mt-sm-0">
+                <img src="<?php echo base_url('argon\assets\img\theme\team-1-800x800.jpg');?>" alt="Raised image" class="img-fluid rounded shadow-lg" style="width: 150px;">
+                <small class="d-block text-uppercase text-white font-weight-bold">Cyntia Fanny</small>
+                <small class="d-block text-uppercase text-white font-weight-bold mb-4">000 000 xxxxx</small>
+          </div>
+        </div>
+        <h2 class="text-warning">Our Back End Team</h2>
+        <div class="row" id="backEnd">
+            <div class="col-sm-4 col-6 mt-4 mt-sm-0">
+                  <img src="<?php echo base_url('argon\assets\img\theme\team-1-800x800.jpg');?>" alt="Raised image" class="img-fluid rounded shadow-lg" style="width: 150px;">
+                  <small class="d-block text-uppercase text-white font-weight-bold">Cindy Michelle</small>
+                  <small class="d-block text-uppercase text-white font-weight-bold mb-4">000 000 xxxxx</small>
+            </div>
+            <div class="col-sm-4 col-6 mt-4 mt-sm-0">
+                  <img src="<?php echo base_url('argon\assets\img\theme\team-1-800x800.jpg');?>" alt="Raised image" class="img-fluid rounded shadow-lg" style="width: 150px;">
+                  <small class="d-block text-uppercase text-white font-weight-bold">Samuel Lorent</small>
+                  <small class="d-block text-uppercase text-white font-weight-bold mb-4">000 000 xxxxx</small>
+            </div>
+            <div class="col-sm-4 col-6 mt-4 mt-sm-0">
+                  <img src="<?php echo base_url('argon\assets\img\theme\team-1-800x800.jpg');?>" alt="Raised image" class="img-fluid rounded shadow-lg" style="width: 150px;">
+                  <small class="d-block text-uppercase text-white font-weight-bold">Handriki Kasa</small>
+                  <small class="d-block text-uppercase text-white font-weight-bold mb-4">000 000 xxxxx</small>
+            </div>
+        </div>
+
+    </div>
+</body>
---
+++ application/views/pages/shop/aboutUs_view.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>About Us</title>
+        <?php echo $css; ?>
+    </head>
+
+        <?php
+            echo $header;
+            echo $us;
+            echo $footer;
+        ?>
+
+        <?php echo $js; ?>
+</html>
